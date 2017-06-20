7a599c1889f2c06eb0a81478efade354f4e85075
laitrongsang95 <1312479@student.hcmus.edu.vn>
2017-06-20 21:41:12 +0700

    update routes

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,5 @@
 var Router = require('express').Router;
 
-
 var controllers = require('../app/controllers');
 
 
@@ -8,26 +7,39 @@ var controllers = require('../app/controllers');
 module.exports = function(app) {
     var homeRoutes = Router()
         .get('/', controllers.home.index);
-
-    var usersRoutes = Router()
-        .get('/', controllers.users.index)
-        .get('/login', controllers.users.login)
-        .post('/login', controllers.users.search)
-        .post('/register', controllers.users.register)
-        .get('/logout', controllers.users.logout)
-        .post('/', controllers.users.addfriend)
-        .get('/listfriend', controllers.users.listfriend)
-        .get('/register', controllers.users.signup);
-    var messagesRoutes = Router()
-        .get('/', controllers.messages.index)
-        .get('/createmessage', controllers.messages.create)
-        .post('/createmessage', controllers.messages.insert)
-        .get('/viewmessage', controllers.messages.view);
     var aboutsRoutes = Router()
         .get('/', controllers.abouts.index);
     app.use('/', homeRoutes);
-    app.use('/users', usersRoutes);
-    app.use('/messages', messagesRoutes);
     app.use('/abouts', aboutsRoutes);
 
+    var blogsRoutes = Router()
+        .get('/', controllers.blogs.index);
+    app.use('/blogs', blogsRoutes);
+    app.use('/', homeRoutes);
+    app.use('/abouts', aboutsRoutes);
+
+    var loginRoutes = Router()
+    	.get('/', controllers.login.index);
+    app.use('/login', loginRoutes);
+    app.use('/blogs', blogsRoutes);
+    app.use('/', homeRoutes);
+    app.use('/abouts', aboutsRoutes);
+
+    var registerRoutes = Router()
+        .get('/', controllers.register.index);
+    app.use('/register', registerRoutes);
+    app.use('/login', loginRoutes);
+    app.use('/blogs', blogsRoutes);
+    app.use('/', homeRoutes);
+    app.use('/abouts', aboutsRoutes);
+
+    var albumsRoutes = Router()
+    	.get('/', controllers.albums.index);
+        
+    app.use('/albums/photo', albumsRoutes);
+    app.use('/albums', albumsRoutes);
+    app.use('/login', loginRoutes);
+    app.use('/blogs', blogsRoutes);
+    app.use('/', homeRoutes);
+    app.use('/abouts', aboutsRoutes);
 };
