f84b3d551b5d4d0985d080f6706fa23379036275
PHP\phpho <1312432@student.hcmus.edu.vn>
2017-06-24 18:07:28 +0700

    kiem tra dang nhap de truy cap cac trang khac

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,26 +15,26 @@ module.exports = function(app) {
     app.use('/public', express.static('public'));
     app.use('/components', express.static('bower_components'));
 
-    // app.use(function(req, res, next) {
-    //     //neu la login ->render
+    app.use(function(req, res, next) {
+        //neu la login ->render
 
-    //     if (req.originalUrl == '/users/login' || req.method != 'GET' || req.originalUrl == '/users/register') {
-    //         console.log(req.originalUrl + "1" + req.method);
-    //         next()
-    //     } else {
-    //         console.log(req.originalUrl + "2" + req.method);
-    //         //truong hop khac
-    //         var username = req.session.userid;
-    //         console.log(req.session);
+        if (req.originalUrl == '/users/signin' || req.method != 'GET' || req.originalUrl == '/users/signup') {
+            console.log(req.originalUrl + "1" + req.method);
+            next()
+        } else {
+            //console.log(req.originalUrl + "2" + req.method);
+            //truong hop khac
+            var username = req.session.userid;
+            //console.log(req.session);
 
-    //         if (username == undefined) {
-    //             res.redirect('/users/login');
-    //         } else {
-    //             next()
-    //         }
-    //         //neu chua co session -> redirect users/login
+            if (username == undefined) {
+                res.redirect('/users/signin');
+            } else {
+                next()
+            }
+            //neu chua co session -> redirect users/login
 
-    //     }
+        }
 
-    // });
+    });
 };
